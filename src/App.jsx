567767c3d77671717f54e681fa5c9785e1e0c472
Jash <v.jashwanth.2005@gmail.com>
2025-07-31 11:59:07 +0530

    Revert "Updated App.jsx"
    This reverts commit abcbd297695edb0aff12a463bb79f6bdd5e48b06.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="container">
       <h1>Welcome to My React Project - S105</h1>
-      <h1>CI/CD pipeline</h1>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
     </div>
   );
